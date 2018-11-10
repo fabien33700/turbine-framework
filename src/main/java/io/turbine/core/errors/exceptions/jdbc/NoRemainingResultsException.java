70971a579817a5fefff2bf3df9a875e26622c6ac
Fabien Le Houëdec <fabien.lehouedec@gmail.com>
2018-11-10 18:40:07 +0100

    Clean up, removed not anymore useful classes.

--- src/main/java/io/turbine/core/errors/exceptions/jdbc/NoRemainingResultsException.java
+++
@@ -1,9 +0,0 @@
-package io.turbine.core.errors.exceptions.jdbc;
-
-public class NoRemainingResultsException extends JdbcException {
-    private static final String DEFAULT_MESSAGE = "All results have been read from this results set.";
-
-    public NoRemainingResultsException() {
-        super(DEFAULT_MESSAGE);
-    }
-}
