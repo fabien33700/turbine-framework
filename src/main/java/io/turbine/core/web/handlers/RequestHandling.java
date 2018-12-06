3ed34509ce8cf7186b264ef67bd483ac2b9c5ef7
Fabien Le Houëdec <fabien.lehouedec@gmail.com>
2018-12-06 12:45:10 +0100

    Merging code clean up

---
+++ src/main/java/io/turbine/core/web/handlers/RequestHandling.java
@@ -0,0 +1,17 @@
+package io.turbine.core.web.handlers;
+
+import io.reactivex.BackpressureStrategy;
+import io.vertx.core.http.HttpMethod;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface RequestHandling {
+    String path() default "/";
+    HttpMethod method() default HttpMethod.GET;
+    BackpressureStrategy strategy() default BackpressureStrategy.DROP;
+}
